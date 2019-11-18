68d2df97181bb124c50defe9b734fa6d84611802
Yann Caperan <yann.caperan@gmail.com>
2019-11-18 16:35:38 +0100

    Viewing model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_18_145034) do
+ActiveRecord::Schema.define(version: 2019_11_18_153230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,4 +27,16 @@ ActiveRecord::Schema.define(version: 2019_11_18_145034) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "viewings", force: :cascade do |t|
+    t.string "movie_info"
+    t.integer "capacity"
+    t.date "date"
+    t.integer "price"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_viewings_on_user_id"
+  end
+
+  add_foreign_key "viewings", "users"
 end
