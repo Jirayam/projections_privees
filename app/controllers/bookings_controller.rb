5fbd08fd0efda6aa7fd2677a34e6f945da53da2c
Mickael Durand <micdurand@gmail.com>
2019-11-20 15:16:33 +0100

    first version of booking pages ok

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,21 @@
 class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+    @booking = Booking.find(params[:viewing_id])
+    @booking.user_id = current_user
+    @viewing = @booking.viewing
+  end
+
   def create
     @booking = Booking.new(booking_params)
+    @viewing = @booking.viewing
     @booking.user = current_user
     if @booking.valid?
       @booking.save
-      redirect_to viewing_path(@booking.viewing)
+      redirect_to viewing_booking_path(@booking, @viewing)
     else
       render :new
     end
